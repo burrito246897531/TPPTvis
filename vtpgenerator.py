79eb07f8923090408e28b25b064ba802aa062bc4
Kale Chen <burrito246897531@gmail.com>
2026-04-03 23:12:08 -0500

    04/03 anthropomorphic phantom

--- vtpgenerator.py
+++ vtpgenerator.py
@@ -12,8 +12,8 @@ Examples are given below, vtpgenerator_utils.py contains functions for:
 import vtpgenerator_utils as vg
 
 # Example 1: Ge68 line source, 2mm radius, 100mm length, centered at (0, 0, 0) along the z axis
-#ge68_line_source = vg.generate_cylinder_mesh(0, 0, 0, 2, 100, direction=(0, 0, 1), resolution=64)
-#vg.save_mesh_as_vtp(ge68_line_source, 'ge68_line_source3.vtp')
+ge68_line_source = vg.generate_cylinder_mesh(0, 0, 0, 2, 100, direction=(0, 0, 1), resolution=64)
+vg.save_mesh_as_vtp(ge68_line_source, 'ge68_line_source.vtp')
 
 #cube_source = vg.generate_rectangular_prism_mesh(0, 0, 0, 10, 10, 10)
 #vg.save_mesh_as_vtp(cube_source, 'cube_source.vtp')
@@ -28,5 +28,5 @@ import vtpgenerator_utils as vg
 #castor_mesh = vg.castor_mesh(file_name + '.img', file_name + '.hdr', prop = 0.0008, trim = 56, color_map = 'hot_r', opacity_map_func = vg.get_opacity_09cubic)
 #vg.save_mesh_as_vtp(castor_mesh, 'd1d0008prop09cubic.vtp')
 
-derenzo_mesh = vg.generate_derenzo_mesh(0, 0, 0, 1.59 / 2, 100, angle = 270,resolution=64, color = [255, 0, 0] )
-vg.save_mesh_as_vtp(derenzo_mesh, 'derenzo2mm.vtp')
+#derenzo_mesh = vg.generate_derenzo_mesh(0, 0, 0, 1.59 / 2, 100, angle = 270,resolution=64, color = [255, 0, 0] )
+#vg.save_mesh_as_vtp(derenzo_mesh, 'derenzo2mm.vtp')
